f6d4b85f92a88a7c248d0cf59a501a660a277098
lsolesen <lars@intraface.dk>
2016-03-24 19:57:40 +0000

    Added TravisCI integration

---
+++ tests/TemplateTest.php
@@ -0,0 +1,10 @@
+<?php
+require_once dirname(__FILE__) . '/../src/Template.php';
+
+class TemplateTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $this->markTestIncomplete('Test is not implemented');
+    }
+}
